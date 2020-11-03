010d920980732a989db307d15ae8bfb046c9f612
verityyt <joshua-schmidt@online.de>
2020-11-03 17:19:08 +0100

    :bookmark: Change version number (new beta-release)

--- src/main/kotlin/HardwareMonitoringDisplay.kt
+++ src/main/kotlin/HardwareMonitoringDisplay.kt
@@ -39,7 +39,7 @@ object HardwareMonitoringDisplay {
     /**
      * Version of the Application
      */
-    val version = "v0.1.8"
+    val version = "v0.2.1"
 
     @JvmStatic
     fun main(args: Array<String>) {
