252be5c9d076f5ac5acc09ff0f98001c5f4e7f3b
Verity <joshua-schmidt@online.de>
2021-04-02 15:03:36 +0200

    :bug: Fix creation of configuration

--- src/main/kotlin/backend/Configuration.kt
+++ src/main/kotlin/backend/Configuration.kt
@@ -1,36 +1,49 @@
 package backend
 
+import org.gradle.internal.impldep.com.fasterxml.jackson.databind.util.JSONPObject
 import org.json.simple.JSONObject
 import org.json.simple.parser.JSONParser
 import java.io.File
+import java.io.FileNotFoundException
 
 object Configuration {
 
     val file = File("config.json")
 
     fun check() {
-        val json = JSONParser().parse(file.readText()) as JSONObject
+        try {
+            val json = JSONParser().parse(file.readText()) as JSONObject
 
-        if(json["alarm_cpu"] == null) {
-            Logger.log("First start", this.javaClass)
-
-            json["language"] = "en"
-            json["style"] = "0"
-            json["update_delay_ms"] = "1000"
-            json["color_cpu"] = "#3867D6"
-            json["color_gpu"] = "#8854D0"
-            json["color_ram"] = "#2ECC71"
-            json["color_drive1"] = "#E67E22"
-            json["color_drive2"] = "#F1C40F"
-            json["alarm_sound"] = "false"
-            json["alarm_cpu"] = "0"
-            json["alarm_gpu"] = "0"
-            json["posX"] = "0"
-            json["posY"] = "0"
-            json["fullscreen"] = "false"
-
-            file.writeText(json.toJSONString())
+            if (json["alarm_cpu"] == null) {
+                createConfig()
+            }
+        } catch (e: FileNotFoundException) {
+            createConfig()
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
+    }
+
+    private fun createConfig() {
+        val json = JSONObject()
+        Logger.log("Config file does not exists", this.javaClass)
+
+        json["language"] = "en"
+        json["style"] = "0"
+        json["update_delay_ms"] = "1000"
+        json["color_cpu"] = "#3867D6"
+        json["color_gpu"] = "#8854D0"
+        json["color_ram"] = "#2ECC71"
+        json["color_drive1"] = "#E67E22"
+        json["color_drive2"] = "#F1C40F"
+        json["alarm_sound"] = "false"
+        json["alarm_cpu"] = "0"
+        json["alarm_gpu"] = "0"
+        json["posX"] = "0"
+        json["posY"] = "0"
+        json["fullscreen"] = "false"
+
+        file.writeText(json.toJSONString())
     }
 
     fun get(key: String): String {
